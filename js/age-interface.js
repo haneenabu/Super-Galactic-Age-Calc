d7d9919b0cfa570ef1b3b0d2be789db74aa400d3
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-09-08 11:25:18 -0700

    fix webpage to display the age entered in seconds

--- js/age-interface.js
+++ js/age-interface.js
@@ -1,13 +1,16 @@
-//import{AgeCalc} from './../js/ageCalc.js';
+import { AgeCalc } from './../js/ageCalc.js';
+
 $(document).ready(function() {
   $('#ageConverter').submit(function(event) {
+
     event.preventDefault();
     let input = $('#age').val();
     if (isNaN(input)) {
       alert("You did not enter a number");
     } else{
-      let output = AgeCalc(input);
-      $('#ageInSecResult').text(output);
+      let output = new AgeCalc(input);
+      $('#ageInSecResult').text(output.ageToSec(input));
+
     }
   });
 });
--- spec/age-spec.js
+++ spec/age-spec.js
@@ -6,5 +6,16 @@ describe('AgeCalc', function(){
     let ageCalc = new AgeCalc("3");
     expect(ageCalc.age).toEqual("3");
   });
-
+  it('should calculate number of seconds in 1 year', function(){
+    let ageCalc = new AgeCalc("1");
+    expect(ageCalc.ageToSec("1")).toEqual(31556952);
+  });
+  it('should calculate number of seconds in 10 year', function(){
+    let ageCalc = new AgeCalc("10");
+    expect(ageCalc.ageToSec("10")).toEqual(315569520);
+  });
+  it('should calculate number of seconds in 27 year', function(){
+    let ageCalc = new AgeCalc("27");
+    expect(ageCalc.ageToSec("27")).toEqual(852037704);
+  });
 });
